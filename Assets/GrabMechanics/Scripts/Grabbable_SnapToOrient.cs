4d6b7c802c511a04ecc21dde09d69d3d5a9fa6bb
James Taylor <taylorjames9@gmail.com>
2017-09-11 19:54:43 -0700

    added snap to orient sample

--- Assets/GrabMechanics/Scripts/Grabbable_SnapToOrient.cs
+++ Assets/GrabMechanics/Scripts/Grabbable_SnapToOrient.cs
@@ -2,15 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grabbable_SnapToOrient : MonoBehaviour {
+public class Grabbable_SnapToOrient : Base_Grab
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    protected override void StartGrab(Grabber grabber1)
+    {
+        base.StartGrab(grabber1);
+        Debug.Log("Did base line.");
+        transform.SetParent(grabber1.transform);
+        Debug.Log("Did set parent.");
+
+        gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        Debug.Log("Did isKinematic line.");
+
+        transform.rotation = transform.parent.rotation;
+        Debug.Log("Did rotate line.");
+
+    }
+
+    protected override void EndGrab(Grabber grabber1)
+    {
+
+        base.EndGrab(grabber1);
+        Debug.Log("This is WHERE WE SHOULD BE RELEASING");
+        //if (myOriginalParent == null)
+        //    transform.SetParent(null);
+        //else
+        //    transform.SetParent(myOriginalParent);
+        transform.SetParent(null);
+        gameObject.GetComponent<Rigidbody>().isKinematic = false;
+    }
+
+
+
+
+
 }
